01e619cae276a95e7993528d27204d3a02fd761e
Himanjal saha <himanjalsaha97@gmail.com>
2024-06-29 15:58:58 +0530

    cors

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
 const dotenv = require('dotenv');
+const cors = require('cors');
 
 dotenv.config();
 
 const prisma = new PrismaClient();
 const app = express();
-const port = process.env.PORT ||  3000;
+const port = process.env.PORT || 3000;
 
+app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 
 app.get('/snippets', async (req, res) => {
@@ -38,49 +40,45 @@ app.post('/snippets', async (req, res) => {
   }
 });
 
-// DELETE endpoint to delete a snippet by ID
 app.delete('/snippets/:id', async (req, res) => {
-    const { id } = req.params;
-  
-    try {
-      const snippet = await prisma.snippet.delete({
-        where: { id },
-      });
-  
-      if (!snippet) {
-        return res.status(404).json({ error: 'Snippet not found' });
-      }
-  
-      res.json({ message: 'Snippet deleted successfully' });
-    } catch (error) {
-      console.error('Error deleting snippet:', error);
-      res.status(500).json({ error: 'Failed to delete snippet' });
-    }
-  });
+  const { id } = req.params;
 
-  // PUT endpoint to update a snippet by ID
-app.put('/snippets/:id', async (req, res) => {
-    const { id } = req.params;
-    const { title, code, language } = req.body;
-  
-    try {
-      const updatedSnippet = await prisma.snippet.update({
-        where: { id },
-        data: {
-          title,
-          code,
-          language,
-        },
-      });
-  
-      res.json(updatedSnippet);
-    } catch (error) {
-      console.error('Error updating snippet:', error);
-      res.status(500).json({ error: 'Failed to update snippet' });
+  try {
+    const snippet = await prisma.snippet.delete({
+      where: { id },
+    });
+
+    if (!snippet) {
+      return res.status(404).json({ error: 'Snippet not found' });
     }
-  });
-  
-  
+
+    res.json({ message: 'Snippet deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting snippet:', error);
+    res.status(500).json({ error: 'Failed to delete snippet' });
+  }
+});
+
+app.put('/snippets/:id', async (req, res) => {
+  const { id } = req.params;
+  const { title, code, language } = req.body;
+
+  try {
+    const updatedSnippet = await prisma.snippet.update({
+      where: { id },
+      data: {
+        title,
+        code,
+        language,
+      },
+    });
+
+    res.json(updatedSnippet);
+  } catch (error) {
+    console.error('Error updating snippet:', error);
+    res.status(500).json({ error: 'Failed to update snippet' });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
